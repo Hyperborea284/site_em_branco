eaabcaafa14689e131381b750cee07c6d255bf71
Abraxas220 <000jhs@gmail.com>
2024-07-27 16:06:39 -0300

    versão aparentemente funcional, reinserir verificação do conteúdo html em sent_beyes e css do painel admin

--- backend/sent_bayes.py
+++ backend/sent_bayes.py
@@ -59,15 +59,6 @@ class SentimentAnalyzer:
         }
         self.generated_images_dir = Path(settings.MEDIA_ROOT) / 'generated_images'
         self.generated_images_dir.mkdir(parents=True, exist_ok=True)
-        self.image_paths = {}
-
-    def set_image_paths(self, timestamp: str):
-        """Define os caminhos das imagens geradas."""
-        self.image_paths = {
-            'pie_chart': self.generated_images_dir / f'pie_chart_{timestamp}.png',
-            'bar_chart': self.generated_images_dir / f'bar_chart_{timestamp}.png',
-            'line_chart': self.generated_images_dir / f'emotion_scores_{timestamp}.png'
-        }
 
     def process_document(self, filepath: str) -> tuple:
         """Processa um documento para separar em parágrafos e frases.
@@ -203,12 +194,14 @@ class SentimentAnalyzer:
         result = classificador.prob_classify(new_features)
         return np.array([result.prob(emotion) for emotion in self.emotions_funcs.keys()])
 
-    def plot_emotion_line_charts(self, scores_list: list, paragraph_end_indices: list):
+    def plot_emotion_line_charts(self, scores_list: list, paragraph_end_indices: list, timestamp: str):
         """Plota um gráfico de barras da distribuição de emoções.
 
         Args:
             scores_list (list): Lista de pontuações de emoções.
             paragraph_end_indices: (list): Lista indicando fim dos parágrafos.
+            timestamp (str): Carimbo de data/hora para nomear os arquivos salvos.
+
         """        
         emotions = list(self.emotions_funcs.keys())
         fig, axes = plt.subplots(nrows=len(emotions), ncols=1, figsize=(10, 2 * len(emotions)))
@@ -221,14 +214,16 @@ class SentimentAnalyzer:
             axes[i].set_xlabel('Contagem de frases')
             axes[i].set_ylabel('Pontuações')
         plt.tight_layout()
-        plt.savefig(self.image_paths['line_chart'])
+        plt.savefig(self.line_chart_path)
         plt.close()
 
-    def plot_pie_chart(self, scores_list: list):
+    def plot_pie_chart(self, scores_list: list, timestamp: str):
         """Plota um gráfico de pizza da distribuição de emoções.
 
         Args:
             scores_list (list): Lista de pontuações de emoções.
+            timestamp (str): Carimbo de data/hora para nomear os arquivos salvos.
+
         """
         labels = list(self.emotions_funcs.keys())
         sizes = [np.mean([score[idx] for score in scores_list]) for idx in range(len(labels))]
@@ -236,14 +231,16 @@ class SentimentAnalyzer:
         plt.figure(figsize=(8, 8))
         plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
         plt.title('Proporção de Cada Sentimento')
-        plt.savefig(self.image_paths['pie_chart'])
+        plt.savefig(self.pie_chart_path)
         plt.close()
 
-    def plot_bar_chart(self, scores_list: list):
+    def plot_bar_chart(self, scores_list: list, timestamp: str):
         """Plota um gráfico de barras das frequências de emoções.
 
         Args:
             scores_list (list): Lista de pontuações de emoções.
+            timestamp (str): Carimbo de data/hora para nomear os arquivos salvos.
+
         """        
         labels = list(self.emotions_funcs.keys())
         sizes = [np.sum([score[idx] for score in scores_list]) for idx in range(len(labels))]
@@ -252,13 +249,22 @@ class SentimentAnalyzer:
         plt.title('Frequência de Emoções Detectadas')
         plt.xlabel('Emoção')
         plt.ylabel('Frequência')
-        plt.savefig(self.image_paths['bar_chart'])
+        plt.savefig(self.bar_chart_path)
         plt.close()
 
     def generate_html_content(self, timestamp: str, paragraphs: list, sentences: list) -> str:
-        """Cria uma visualização HTML dos dados analisados, destacando o final de cada frase e incluindo numeração."""
-        base_path = settings.MEDIA_URL + 'generated_images/'
+        """Cria uma visualização HTML dos dados analisados, destacando o final de cada frase e incluindo numeração.
     
+        Args:
+            timestamp (str): Carimbo de data/hora da análise.
+            paragraphs (list): Lista de parágrafos do texto analisado.
+            sentences (list): Lista de frases do texto analisado.
+        """       
+        base_path = settings.MEDIA_URL + 'generated_images/'
+        self.pie_chart_path = Path(settings.MEDIA_ROOT) / f'generated_images/pie_chart_{timestamp}.png'
+        self.bar_chart_path = Path(settings.MEDIA_ROOT) / f'generated_images/bar_chart_{timestamp}.png'
+        self.line_chart_path = Path(settings.MEDIA_ROOT) / f'generated_images/emotion_scores_{timestamp}.png'
+
         # Constrói o conteúdo HTML do texto analisado com marcações de fim de frase e numeração
         html_paragraphs = []
         sentence_counter = 1
@@ -269,28 +275,19 @@ class SentimentAnalyzer:
                     marked_paragraph = marked_paragraph.replace(sentence, f'{sentence} <span style="color:red;">[{sentence_counter}]</span>')
                     sentence_counter += 1
             html_paragraphs.append(f'<p>{marked_paragraph}</p>')
-    
+
         # Junta os elementos de html_paragraphs em uma string única
         html_content_paragraphs = ''.join(html_paragraphs)
         
         # Constrói o conteúdo HTML do texto
-        html_content = ''.join([
-            "<h1>Texto Analisado</h1>",
-            f"<div style='border:1px solid black; padding:10px;'>{html_content_paragraphs}</div>",
-            "<h1>Data e Hora da Análise</h1>",
-            f"<div style='border:1px solid black; padding:10px;'>{timestamp}</div>",
-            "<h1>Número de Parágrafos e Frases</h1>",
-            f"<div style='border:1px solid black; padding:10px;'>Parágrafos: {len(paragraphs)}, Frases: {len(sentences)}</div>",
-            "<h1>Gráficos Gerados</h1>",
-            "<div style='border:1px solid black; padding:10px;'>",
-            "<h2>Gráfico de Pizza</h2>",
-            f"<img src='/app{base_path}pie_chart_{timestamp}.png' alt='Gráfico de Pizza'>",
-            "<h2>Gráfico de Barras</h2>",
-            f"<img src='/app{base_path}bar_chart_{timestamp}.png' alt='Gráfico de Barras'>",
-            "<h2>Gráfico de Linhas</h2>",
-            f"<img src='/app{base_path}emotion_scores_{timestamp}.png' alt='Gráfico de Linhas'>",
-            "</div>"
-        ])
+        html_content = f"""
+            <h1>Texto Analisado</h1>
+            <div style='border:1px solid black; padding:10px;'>{html_content_paragraphs}</div>
+            <h1>Data e Hora da Análise</h1>
+            <div style='border:1px solid black; padding:10px;'>{timestamp}</div>
+            <h1>Número de Parágrafos e Frases</h1>
+            <div style='border:1px solid black; padding:10px;'>Parágrafos: {len(paragraphs)}, Frases: {len(sentences)}</div>
+        """
         return html_content
 
     def execute_analysis_text(self, text: str) -> tuple:
@@ -301,11 +298,16 @@ class SentimentAnalyzer:
     
         """
         timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-        self.set_image_paths(timestamp)
+        self.pie_chart_path = self.generated_images_dir / f'pie_chart_{timestamp}.png'
+        self.bar_chart_path = self.generated_images_dir / f'bar_chart_{timestamp}.png'
+        self.line_chart_path = self.generated_images_dir / f'emotion_scores_{timestamp}.png'
         paragraphs, sentences = self.process_text(text)
         scores_list, paragraph_end_indices = self.analyze_paragraphs(paragraphs)
-        self.plot_emotion_line_charts(scores_list, paragraph_end_indices)
-        self.plot_pie_chart(scores_list)
-        self.plot_bar_chart(scores_list)
+        self.plot_emotion_line_charts(scores_list, paragraph_end_indices, timestamp)
+        self.plot_pie_chart(scores_list, timestamp)
+        self.plot_bar_chart(scores_list, timestamp)
         html_content = self.generate_html_content(timestamp, paragraphs, sentences)
-        return html_content
+        relative_paths = ['app/media/generated_images/pie_chart_{timestamp}.png',
+                          'app/media/generated_images/bar_chart_{timestamp}.png',
+                          'app/media/generated_images/emotion_scores_{timestamp}.png']
+        return html_content, relative_paths
